37638f5a5cc8062dfb90c74af58e09777e1e5ce1
Luciana Silva <lucianakarolif@gmail.com>
2025-11-13 17:56:28 -0300

    respostas no bruno

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -2,31 +2,31 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"social-media-go/src/banco"
 	"social-media-go/src/modelos"
 	"social-media-go/src/repositorios"
+	"social-media-go/src/respostas"
 )
 
 // CriarUsuario insere um usuario no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	corpoRequest, erro := io.ReadAll(r.Body)
 	if erro != nil {
-		http.Error(w, "Erro ao ler corpo da requisição", http.StatusBadRequest)
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
 		return
 	}
 
 	var usuario modelos.Usuario
 	if erro := json.Unmarshal(corpoRequest, &usuario); erro != nil {
-		http.Error(w, "Erro ao converter usuario para struct", http.StatusBadRequest)
+		respostas.Erro(w, http.StatusBadRequest, erro)
 		return
 	}
 
 	db, erro := banco.Conectar()
 	if erro != nil {
-		http.Error(w, "Erro ao conectar ao banco de dados", http.StatusInternalServerError)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
 		return
 	}
 	defer db.Close()
@@ -34,12 +34,12 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
 	usuario.ID, erro = repositorio.Criar(usuario)
 	if erro != nil {
-		http.Error(w, "Erro ao inserir usuario no banco", http.StatusInternalServerError)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
 		return
 	}
 
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(fmt.Sprintf("ID inserido: %d", usuario.ID)))
+	respostas.JSON(w, http.StatusCreated, usuario)
+
 }
 
 // BuscarUsuarios busca todos os usuarios salvo no banco
--- src/modelos/Usuario.go
+++ src/modelos/Usuario.go
@@ -1,13 +1,52 @@
 package modelos
 
-import "time"
+import (
+    "errors"
+    "strings"
+    "time"
+)
 
 // Usuario representa um usuario utilizando a rede social
 type Usuario struct {
-	ID uint64 `json:"id,omitempty"`
-	Nome string `json:"nome,omitempty"`
-	Nick string `nick:"id,omitempty"`
-	Email string `json:"email,omitempty"`
-	Senha string `json:"senha,omitempty"`
-	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+    ID       uint64    `json:"id,omitempty"`
+    Nome     string    `json:"nome,omitempty"`
+    Nick     string    `json:"nick,omitempty"`
+    Email    string    `json:"email,omitempty"`
+    Senha    string    `json:"senha,omitempty"`
+    CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+func (usuario *Usuario) preparar() error {
+    if erro := usuario.validar(); erro != nil {
+        return erro
+    }
+
+    usuario.formatar()
+    return nil
+}
+
+func (usuario *Usuario) validar() error {
+    if usuario.Nome == "" {
+        return errors.New("o nome é obrigatório e não pode estar em branco")
+    }
+
+    if usuario.Nick == "" {
+        return errors.New("o nick é obrigatório e não pode estar em branco")
+    }
+
+    if usuario.Email == "" {
+        return errors.New("o email é obrigatório e não pode estar em branco")
+    }
+
+    if usuario.Senha == "" {
+        return errors.New("a senha é obrigatória e não pode estar em branco")
+    }
+
+    return nil
+}
+
+func (usuario *Usuario) formatar() {
+    usuario.Nome = strings.TrimSpace(usuario.Nome)
+    usuario.Nick = strings.TrimSpace(usuario.Nick)
+    usuario.Email = strings.TrimSpace(usuario.Email)
 }
